739c80fa2808f1fa4f63a1f2bd9fdac286aca086
mgwem <101238309+mgwem@users.noreply.github.com>
2022-06-24 06:53:27 +0000

    [Add]コメントの非表示

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,11 @@ class Comment < ApplicationRecord
 
   validates :comment, presence: true, length: {maximum:150}
 
+  # 公開ステータス「公開」のコメントを取得
+  scope :published, -> { where(is_hidden: false) }
+  # created_atカラムを降順に並び替え
+  scope :sorted, -> { order(created_at: :DESC) }
+  # 公開コメントを新着順に並び替え
+  scope :recent_comment, -> { published.sorted }
+
 end
