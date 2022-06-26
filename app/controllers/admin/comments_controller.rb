4c8d96d7c699b19b3dd2906d01eb53cd966c1da5
mgwem <101238309+mgwem@users.noreply.github.com>
2022-06-26 06:13:50 +0000

    [Add]admin/comments新着順 admin/posts新着順

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,7 +2,7 @@ class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @comments = Comment.page(params[:page]).order(id: :DESC)
+    @comments = Comment.page(params[:page]).sorted
   end
 
   def destroy
